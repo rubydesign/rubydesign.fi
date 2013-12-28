5411ff81c7a3dac1e996a8aa7cf0f323ac0d8d3a
Torsten Ruger <torsten@villataika.fi>
2013-12-28 20:51:21 +0200

    move request specs to features ala capybara and fix them

--- spec/features/addresses_spec.rb
+++ spec/features/addresses_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Addresses" do
   describe "GET /addresses" do
     it "lists addresses" do
-      visist addresses_path
+      visit addresses_path
       page.should_not have_css(".translation_missing")
     end
   end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Products" do
   describe "GET /products" do
     it "lists products" do
-      get products_path
+      visit products_path
       page.should_not have_css(".translation_missing")
     end
   end
