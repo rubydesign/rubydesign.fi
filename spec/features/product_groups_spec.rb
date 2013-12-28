cb74c43819369c4148ac1d914d79aa81ad381a51
Torsten Ruger <torsten@villataika.fi>
2013-12-28 22:18:11 +0200

    fix json csv links and remove scope, somw basic locale text

--- spec/features/product_groups_spec.rb
+++ spec/features/product_groups_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 describe "ProductGroups" do
-  describe "GET /product_groups" do
+  describe "/product_groups" do
     it "lists product groups" do
       visit product_groups_path
+      page.should_not have_css(".translation_missing")
+    end
+  end
+  describe "/product_groups/new" do
+    it "creates a new group" do
+      visit new_product_group_path
       page.should_not have_css(".translation_missing")
     end
   end
