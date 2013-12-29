ce002bce66c9d1c8f359e3b0d370518e3d3585f4
Torsten Ruger <torsten@villataika.fi>
2013-12-29 12:27:18 +0200

    more beautiful cleaning, textprocessing goes

--- app/models/product_group.rb
+++ app/models/product_group.rb
@@ -11,17 +11,9 @@ class ProductGroup < ActiveRecord::Base
     order("#{attribute} #{direction}")
   }
 
-  include BeautifulScaffoldModule
-  before_save :fulltext_field_processing
-
   # You can OVERRIDE this method used in model form and search form (in belongs_to relation)
   def caption
     (self["name"] || self["label"] || self["description"] || "##{id}")
-  end
-
-  def fulltext_field_processing
-    # You can preparse with own things here
-    generate_fulltext_field([])
   end
 
   def self.permitted_attributes
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,9 @@ class User < ActiveRecord::Base
     order("#{attribute} #{direction}")
   }
 
-  include BeautifulScaffoldModule
-  before_save :fulltext_field_processing
-
   # You can OVERRIDE this method used in model form and search form (in belongs_to relation)
   def caption
     (self["name"] || self["label"] || self["description"] || "##{id}")
-  end
-
-  def fulltext_field_processing
-    # You can preparse with own things here
-    generate_fulltext_field([])
   end
 
   def self.permitted_attributes
@@ -36,22 +28,7 @@ class User < ActiveRecord::Base
     order("#{attribute} #{direction}")
   }
 
-  include BeautifulScaffoldModule
-  before_save :fulltext_field_processing
 
-  # You can OVERRIDE this method used in model form and search form (in belongs_to relation)
-  def caption
-    (self["name"] || self["label"] || self["description"] || "##{id}")
-  end
-
-  def fulltext_field_processing
-    # You can preparse with own things here
-    generate_fulltext_field([])
-  end
-
-  def self.permitted_attributes
-    return :email,:name
-  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
