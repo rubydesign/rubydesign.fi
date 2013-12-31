729223227371630ab1af7d4f3fc8ed4758f13a4a
Torsten Ruger <torsten@villataika.fi>
2013-12-31 15:58:18 +0200

    improve product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,21 @@
+# RubyClerk product may represent one of three things
+# - the normal product (all attributes work as expected)
+# - a product line, in which case it may not be sold and inventory is meaningless
+#                   other attributes (like price/cost) act as a template and are copied into children
+#                   where they are then used.
+# - a member of a product line, in which case the default view concatenates name and description and 
+#                   shows prices relative to it's parent
+#  
+
+# attributes : see permitted_attributes + inventory + deleted_on
+
 class Product < ActiveRecord::Base
   has_many :products, :dependent => :nullify
+  has_many :images, -> {order(:position)}, :dependent => :destroy
+  belongs_to :product
+  belongs_to :product_group
+  belongs_to :supplier
+
   scope :sorting, lambda{ |options|
     attribute = options[:attribute]
     direction = options[:sorting]
@@ -10,6 +26,25 @@ class Product < ActiveRecord::Base
     order("#{attribute} #{direction}")
   }
 
+  # default product scope only lists non-deleted variants
+  scope :active, lambda { where(:deleted_on => nil) }
+  scope :deleted, lambda { where('deleted_on IS NOT NULL') }
+
+  validates :price, :numericality => true , :presence => true
+  validates :cost_price, :numericality => true
+  validates :name, :presence => true
+  validates :url_name, presence: true, :if => :generate_url_if_needed
+ 
+  def generate_url_if_needed
+    unless url_name
+      url_name = name.gsub(" " , "_").downcase
+    end
+    true
+  end
+    def paid_with_card?
+      payment_type == "card"
+    end
+    
   # You can OVERRIDE this method used in model form and search form (in belongs_to relation)
   def caption
     (self["name"] || self["label"] || self["description"] || "##{id}")
@@ -18,7 +53,4 @@ class Product < ActiveRecord::Base
   def self.permitted_attributes
     return :price,:cost,:weight,:name,:description,:url_name,:ean,:tax,:properties,:scode,:product_id,:product_group_id,:supplier_id
   end
-  belongs_to :product
-  belongs_to :product_group
-  belongs_to :supplier
 end
--- db/migrate/20131226151332_create_products.rb
+++ db/migrate/20131226151332_create_products.rb
@@ -1,20 +1,21 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.float :price
-      t.float :cost
-      t.float :weight
-      t.string :name
-      t.text :description
-      t.string :url_name
-      t.string :ean
-      t.float :tax
+      t.float   :price
+      t.float   :cost , :default => 0.0
+      t.float   :weight , :default => 0.1
+      t.string  :name
+      t.text    :description
+      t.string  :url_name
+      t.string  :ean
+      t.float   :tax
       t.integer :inventory , :default => 0
-      t.string :properties
-      t.string :scode
-      t.references :product, index: true
-      t.references :product_group, index: true
-      t.references :supplier, index: true
+      t.string  :properties
+      t.string  :scode
+      t.date    :deleted_on
+      t.references :product,        index: true
+      t.references :product_group,  index: true
+      t.references :supplier,       index: true
 
       t.timestamps
     end
