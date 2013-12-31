231717d37a1bcf53e041ef394822a2fa151ef357
Torsten Ruger <torsten@villataika.fi>
2013-12-31 21:28:41 +0200

    adds description

--- db/migrate/20131226143612_product_groups.rb
+++ db/migrate/20131226143612_product_groups.rb
@@ -3,6 +3,7 @@ class ProductGroups < ActiveRecord::Migration
     create_table :product_groups do |t|
       t.references :product_group
       t.string      :name
+      t.string      :description
       t.integer     :position
       t.string      :url_name
       t.attachment  :main_picture
