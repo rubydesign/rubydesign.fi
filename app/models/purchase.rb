488d593bf3879cc06d22a065cb8bf5d590c81001
Torsten Ruger <torsten@villataika.fi>
2014-01-05 20:35:22 +0200

    fix purchases and add spec

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,8 @@
 class Purchase < ActiveRecord::Base
   
   belongs_to :basket #wording is wrong, but thats ar for you. should be has_one, but the key is here
-  
+  belongs_to :supplier
+
   scope :sorting, lambda{ |options|
     attribute = options[:attribute]
     direction = options[:sorting]
--- spec/features/purchases_spec.rb
+++ spec/features/purchases_spec.rb
@@ -8,4 +8,21 @@ describe "Purchases" do
       page.should_not have_css(".translation_missing")
     end
   end
+  it "creates a new group" do
+    visit new_purchase_path
+    status_code.should be 200
+    page.should_not have_css(".translation_missing")
+  end
+  it "edit" do
+    @purchase = create(:purchase)
+    visit edit_purchase_path(@purchase)
+    status_code.should be 200
+    page.should_not have_css(".translation_missing")
+  end
+  it "shows" do
+    @purchase = create(:purchase)
+    visit purchase_path(@purchase)
+    status_code.should be 200
+    page.should_not have_css(".translation_missing")
+  end
 end
