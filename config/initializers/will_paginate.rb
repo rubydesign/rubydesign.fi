1d9f6ecab956c47e0db1a57dff3cf74544228257
Torsten Ruger <torsten@villataika.fi>
2014-01-07 15:02:56 +0200

    paginate extracted

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,4 +1,5 @@
+require "i18n"
 WillPaginate::ViewHelpers.pagination_options[:inner_window] = 4
 WillPaginate::ViewHelpers.pagination_options[:outer_window] = 2
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = I18n.t(:prev)
-WillPaginate::ViewHelpers.pagination_options[:next_label]     = I18n.t(:next)
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = "prev"
+WillPaginate::ViewHelpers.pagination_options[:next_label]     = "next"
