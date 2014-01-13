2285bd88b0f8aff83274ea011ad2386fa63d4ebc
Torsten Ruger <torsten@villataika.fi>
2014-01-13 18:04:41 +0200

    catch 400s on edit

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -8,4 +8,19 @@ describe "Products" do
       page.should_not have_css(".translation_missing")
     end
   end
+  describe "new product" do
+    it "should render" do
+      visit new_product_path
+      status_code.should be 200
+      page.should_not have_css(".translation_missing")
+    end
+  end
+  describe "edit product" do
+    it "should render" do
+      product = create :product
+      visit edit_product_path product
+      status_code.should be 200
+      page.should_not have_css(".translation_missing")
+    end
+  end
 end
