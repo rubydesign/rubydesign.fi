f5331dba33cfcd134996ec5e4393a7bdd5658e3a
Torsten Ruger <torsten@villataika.fi>
2014-01-23 14:50:49 +0200

    fix the order number

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,21 @@ class Order < ActiveRecord::Base
 
   store :address, accessors: [ :name , :street , :city , :phone ] , coder: JSON
 
+  before_validation :generate_order_number, :on => :create
+
   default_scope { order('created_at DESC').includes(:basket) }
+
+  # many a european goverment requires buisnesses to have running order/transaction numbers.
+  # this is what we use, but it can easily be changed by redifining this method
+  # format RYYYYRUNIN  R, 4 digit year and a running number
+  def generate_order_number
+    if last = Order.first # last, but default order is reversed
+      num = last.number[5..9].to_i + 1
+    else
+      num = 30000
+    end
+    self.number = "R#{Time.now.year}#{num}"
+  end
 
   def total_price
     basket.total_price + shipping_price
