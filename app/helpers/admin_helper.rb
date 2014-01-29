57b854ffd08ebe50bf5a6e31e38e0967af978449
Torsten Ruger <torsten@villataika.fi>
2014-01-29 15:51:47 +0200

    conditional link for baskets

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,6 +1,20 @@
 # encoding : utf-8
 module AdminHelper
 
+  def basket_edit_link basket , options = {}
+    text = t(:edit)
+    link= basket_path(basket)
+    case basket.kori_type
+    when "Order"
+      text = I18n.t(:order)
+      link = order_path(basket.kori)
+    when "Purchase"
+      text = I18n.t(:order)
+      link = purchase_path(basket.kori)
+    end
+    return link_to text , link , options 
+  end
+  
   def column_style(model_name, field_name, display_default = 'table-cell', other_css = "")
     return :class => ""
   end
