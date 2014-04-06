964293d713d58e682d2fcf6d645692520c8a45fb
Torsten Ruger <torsten@villataika.fi>
2014-04-06 21:14:59 +0300

    fix link for dangling associations

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -8,7 +8,7 @@ module AdminHelper
     case basket.kori_type
     when "Order"
       text = I18n.t(:order)
-      link = order_path(basket.kori)
+      link = order_path(basket.kori) rescue ""
     when "Purchase"
       text = I18n.t(:purchase)
       link = purchase_path(basket.kori)
