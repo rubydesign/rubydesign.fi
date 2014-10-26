33ce72379f7c49d7c5db2b9e6dd5a2df073a0782
Torsten Ruger <torsten@villataika.fi>
2014-10-26 14:16:06 +0200

    implement the other product list in shop product page

--- app/helpers/shop_helper.rb
+++ app/helpers/shop_helper.rb
@@ -7,4 +7,11 @@ module ShopHelper
     end
     parents.reverse
   end
+  def in_group( product , get = 4)
+    group = product.category
+    return [] unless group
+    prods = group.products.to_a
+    prods.delete(product)
+    prods.sample(get)
+  end
 end
