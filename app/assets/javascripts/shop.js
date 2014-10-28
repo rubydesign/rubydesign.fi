b7293dc1fc6aa4273d9c91fa04467207d5f998ef
Torsten Ruger <torsten@villataika.fi>
2014-10-28 14:03:53 +0200

    improve chekout form and make js to fill old address

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -13,6 +13,16 @@ function shipmentSelected(){
   $("#order_total").text( parseFloat(ship.replace(",", ".")) + parseFloat(basket.replace(",", ".")));
 }
 
+function fillAddress(){
+  old = $("#previous_address");
+  target = $("#address_form");
+  target.find("#name").val( old.find(".name").text() )
+  target.find("#street").val( old.find(".street").text() )
+  target.find("#city").val( old.find(".city").text() )
+  target.find("#phone").val( old.find(".phone").text() )
+}
+
 $(function() {
-   $("input[name='shipmentOption']").click(shipmentSelected);
+  $("input[name='shipmentOption']").click(shipmentSelected);
+  $("#fill_address").click(fillAddress);
 });
