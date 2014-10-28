59f67e90e610f31238fb573214771dae5d4e48d3
Torsten Ruger <torsten@villataika.fi>
2014-10-28 11:14:33 +0200

    add selected shipping costs on checkout page

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -6,3 +6,13 @@
 //= require bootstrap
 //= require_self
 
+function shipmentSelected(){
+  ship = $("input[name='shipmentOption']:checked").val();
+  basket = $("#basket_total").text();
+  $("#shipping_cost").text( ship );
+  $("#order_total").text( parseFloat(ship.replace(",", ".")) + parseFloat(basket.replace(",", ".")));
+}
+
+$(function() {
+   $("input[name='shipmentOption']").click(shipmentSelected);
+});
