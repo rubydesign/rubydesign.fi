d82c8768f63ae1957d068c3f9ad8ee8f6d28c5e8
Torsten Ruger <torsten@villataika.fi>
2014-10-29 09:48:25 +0200

    rename fields for order and fix transfer

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -18,10 +18,10 @@ function shipmentSelected(){
 function fillAddress(){
   old = $("#previous_address");
   target = $("#address_form");
-  target.find("#name").val( old.find(".name").text() )
-  target.find("#street").val( old.find(".street").text() )
-  target.find("#city").val( old.find(".city").text() )
-  target.find("#phone").val( old.find(".phone").text() )
+  target.find("#order_name").val( old.find(".name").text() )
+  target.find("#order_street").val( old.find(".street").text() )
+  target.find("#order_city").val( old.find(".city").text() )
+  target.find("#order_phone").val( old.find(".phone").text() )
 }
 
 $(function() {
