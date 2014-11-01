25118166a1ec0c2514ef221202386896c6657bfb
Torsten Ruger <torsten@villataika.fi>
2014-11-01 20:25:24 +0200

    remove instance variables from specs

--- spec/features/baskets/edit_spec.rb
+++ spec/features/baskets/edit_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe "edit baskets" do
   before :each do
     sign_in
-    @basket = create :basket
-    @basket.items << create(:item22)
-    @basket.save!
-    visit_path edit_basket_path(@basket)
   end
   it "renders item too" do
+    basket = create :basket_2_items
+    visit_path edit_basket_path(basket)
     td = find(".table").find(".name")
-    td.should have_content(@basket.items.first.name)
+    expect(td).to have_content(@basket.items.first.name)
   end
   it "deletes item" do
-    name = @basket.items.first.name
+    basket = create :basket_2_items
+    visit_path edit_basket_path(basket)
+    name = basket.items.first.name
     within(".table") do
       click_link I18n.t(:delete)
     end
@@ -24,11 +24,15 @@ describe "edit baskets" do
     expect(page).not_to have_content(name )
   end
   it "goes to purchase" do
+    basket = create :basket_2_items
+    visit_path edit_basket_path(basket)
     click_on I18n.t(:make_purchase)
-    find("h2").should have_content(I18n.t(:purchase))
+    expect(find("h2")).to have_content(I18n.t(:purchase))
   end
   it "finds print link" do
-    page.should have_content(I18n.t(:print_order))
-    find(".print_order").should have_content(I18n.t(:print_order))
+    basket = create :basket_2_items
+    visit_path edit_basket_path(basket)
+    expect(page).to have_content(I18n.t(:print_order))
+    expect(find(".print_order")).to have_content(I18n.t(:print_order))
   end
 end
