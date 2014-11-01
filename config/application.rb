8957d3495d91058176b2d65dd1d2a2f6ffccef97
Torsten Ruger <torsten@villataika.fi>
2014-11-01 15:51:59 +0200

    minor

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,6 @@ module OfficeClerk
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
 
-
-      g.view_specs false
       g.helper_specs false
     end
     config.exceptions_app = self.routes
