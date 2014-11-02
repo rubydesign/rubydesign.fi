f1975233bf2f33f738a1948575c94332fa4df514
Torsten Ruger <torsten@villataika.fi>
2014-11-02 14:42:49 +0200

    improve the basket link in summary

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,15 +3,16 @@ module AdminHelper
 
   def basket_edit_link basket , options = {}
     return "---" unless basket
-    text = t(:edit)
-    link= edit_basket_path(basket)
+    return "" unless request.url.include?("basket")
+    text = t(:edit) + " "
+    link = edit_basket_path(basket)
     case basket.kori_type
     when "Order"
-      text = I18n.t(:order)
+      text += I18n.t(:order)
       link = order_path(basket.kori) rescue ""
     when "Purchase"
-      text = I18n.t(:purchase)
-      link = purchase_path(basket.kori)
+      text += I18n.t(:purchase)
+      link = purchase_path(basket.kori) rescue ""
     end
     return link_to text , link , options 
   end
