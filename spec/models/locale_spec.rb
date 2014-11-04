bb22340ff8bb57587e72eddbf294bd7a1712d5b7
Torsten Ruger <torsten@villataika.fi>
2014-11-04 21:48:57 +0200

    adds locale specs

---
+++ spec/models/locale_spec.rb
@@ -0,0 +1,17 @@
+require "i18n-spec"
+
+RSpec.describe 'locale:' do
+
+  Dir.glob('config/locales/*.yml') do |locale_file|
+    next if locale_file == "config/locales/config.yml"
+    context locale_file do
+      it { is_expected.to be_parseable }
+      it { is_expected.to have_valid_pluralization_keys }
+      it { is_expected.not_to have_missing_pluralization_keys }
+      it { is_expected.to have_one_top_level_namespace }
+      it { is_expected.not_to have_legacy_interpolations }
+      it { is_expected.to have_a_valid_locale }
+      it { is_expected.to be_a_complete_translation_of 'config/locales/en.yml' }
+    end
+  end
+end
