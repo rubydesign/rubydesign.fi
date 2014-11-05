8dafc9fe7286913ebfc849f234042363e4974086
Torsten Ruger <torsten@villataika.fi>
2014-11-05 17:22:44 +0200

    fix my horrible js igrnorance

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -7,21 +7,21 @@
 //= require_self
 
 function shipmentSelected(){
-  val = $("input[name='order[shipment_type]']:checked").data("price");
-  ship = parseFloat(val);
+  var val = $("input[name='order[shipment_type]']:checked").data("price");
+  var ship = parseFloat(val);
   val = $("#basket_total").text();
-  basket = parseFloat(val);
+  var basket = parseFloat(val);
   $("#shipping_cost").text( ship.toFixed(2) );
   $("#order_total").text( (ship + basket).toFixed(2) );
 }
 
 function fillAddress(){
-  old = $("#previous_address");
-  target = $("#address_form");
-  target.find("#order_name").val( old.find(".name").text() )
-  target.find("#order_street").val( old.find(".street").text() )
-  target.find("#order_city").val( old.find(".city").text() )
-  target.find("#order_phone").val( old.find(".phone").text() )
+  var old = $("#previous_address");
+  var target = $("#address_form");
+  target.find("#order_name").val( old.find(".name").text() );
+  target.find("#order_street").val( old.find(".street").text() );
+  target.find("#order_city").val( old.find(".city").text() );
+  target.find("#order_phone").val( old.find(".phone").text() );
 }
 
 $(function() {
