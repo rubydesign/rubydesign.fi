49724ffeb230bcf04147423d033d5ea0b75506e3
Torsten Ruger <torsten@villataika.fi>
2014-11-07 18:18:32 +0200

    improve purchase spec

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -5,9 +5,18 @@ describe Purchase do
     p = build :purchase
     expect(p.save).to be  true
   end
+  it "orders ok" do
+    p = create :purchase
+    expect(p.order!).to be true
+  end
   it "receives ok" do
     p = create :purchase
     expect(p.receive!).to be 1
+  end
+  it "doesn't receives twice" do
+    p = create :purchase
+    expect(p.receive!).to be 1
+    expect{p.receive!}.to raise_error RuntimeError 
   end
   it "inventories ok" do
     p = create :purchase
