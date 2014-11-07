5817a388f268b0fcc1ceff6013574b349d83b20b
Torsten Ruger <torsten@villataika.fi>
2014-11-07 22:20:46 +0200

    test for shipping

---
+++ spec/models/shipping_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "ShippingMethod"  do
+  it "has at least one method with data" do
+    methods = OfficeClerk::ShippingMethod.all
+    expect(methods).not_to be_empty
+    expect(methods.values.first.data).to be_a(Hash)
+  end
+  it "provides pickup" do
+    method = OfficeClerk::ShippingMethod.all[:pickup]
+    expect(method).not_to be nil
+    expect(method.name).not_to be nil
+    expect(method.type).not_to be nil
+  end
+  it "Pickup is free" do
+    method = OfficeClerk::ShippingMethod.all[:pickup]
+    basket = create :basket
+    expect(method.price_for(basket)).to be 0.0
+  end
+  it "creates Pickup with data" do
+    method = OfficeClerk::Pickup.new(:name => "me" , :type => :pick2)
+    expect(method.name).to eq "me"
+    expect(method.type).to eq :pick2
+  end
+end
