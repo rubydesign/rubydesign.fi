ee24dc3cbf5c5c58ee943d64f056138279b35b0f
Torsten Ruger <torsten@villataika.fi>
2014-11-09 00:37:22 +0200

    most of the shop cases covered now

--- spec/features/shop_spec.rb
+++ spec/features/shop_spec.rb
@@ -30,10 +30,34 @@ describe ShopController  do
     click_button "add-to-cart-button"
     ensure_path shop_group_path(prod.category.link)
   end
-  it "adds a product" do
+  it "adds a product to checkout" do
     prod = create :shop_product
     visit_path shop_product_path(prod.link)
     click_button "add-to-cart-button"
     visit_path shop_checkout_path
+  end
+  it "fails checkout without email" do
+    prod = create :shop_product
+    visit_path shop_product_path(prod.link)
+    click_button "add-to-cart-button"
+    visit_path shop_checkout_path
+    click_button "make_order"
+    expect(page).to have_content("Error")
+    ensure_path shop_checkout_path
+  end
+  it "checks out with email" do
+    prod = create :shop_product
+    visit_path shop_product_path(prod.link)
+    click_button "add-to-cart-button"
+    visit_path shop_checkout_path
+    fill_in :order_email , :with => "some@valid.it"
+    click_button "make_order"
+    ensure_path shop_order_path(Order.last.id)
+  end
+  it "shows a page" do
+    visit_path shop_page_path :tuotteista
+  end
+  it "renders welcome page" do
+    visit_path shop_welcome_path
   end
 end
