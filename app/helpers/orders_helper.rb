fd55560874d4795d8656e4b1b626598170d8947c
Torsten Ruger <torsten@villataika.fi>
2014-11-09 13:34:21 +0200

    configure and translate order print buttons

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,4 +1,10 @@
 # encoding : utf-8
 require "admin_helper"
 module OrdersHelper
+  def print_styles
+    OfficeClerk.config(:print_styles).split
+  end
+  def print_path style
+    eval("#{style}_order_path(@order)")
+  end
 end
