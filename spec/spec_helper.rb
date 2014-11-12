df49fb84a38774305e1e16edd098606c7ebd720b
Torsten Ruger <torsten@villataika.fi>
2014-11-12 17:36:05 +0200

    fixing url helpers for the engine to work (and test)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   
   config.include Capybara::DSL  
   
-  config.include Rails.application.routes.url_helpers
+  config.include OfficeClerk::Engine.routes.url_helpers
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
