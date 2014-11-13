6e9eb150e04b09e87497623e78da45ffcbd8cbd2
Torsten Ruger <torsten@villataika.fi>
2014-11-13 09:12:03 +0200

    avoid creating empty baskets just for menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_clerk , :current_basket
+  helper_method :current_clerk , :current_basket , :current_basket_or_nil
 
   # users are stored in the session by email
   # if user is not logged i , return nil
@@ -17,15 +17,20 @@ class ApplicationController < ActionController::Base
     redirect_to "/"
   end
 
+  def current_basket_or_nil
+    return @current_basket unless @current_basket.nil?
+    if session[:current_basket]
+      Basket.where( :id => session[:current_basket] ).limit(1).first 
+    else
+      nil
+    end
+  end
   # the current user has a shopping basket which is also stored in the session
   # we *always* return a basket, even if we have to create one (and then store in the session)
   # this is not associated with the user until an order is finalized at which point the order gets the users email (not id)
   # that way people don't have to log in to order, but if they are we can retrieve their orders by email
   def current_basket
-    return @current_basket unless @current_basket.nil?
-    if session[:current_basket]
-      @current_basket = Basket.where( :id => session[:current_basket] ).limit(1).first 
-    end
+    @current_basket = current_basket_or_nil
     if @current_basket.nil?
       @current_basket = Basket.new(:kori_type => "Order")
       @current_basket.save!
