9c87bc477435824a3dce64af3a864e6750cc36d5
Torsten Ruger <torsten@villataika.fi>
2014-11-17 12:29:34 +0200

    ad support for short desciption

--- app/helpers/shop_helper.rb
+++ app/helpers/shop_helper.rb
@@ -1,6 +1,7 @@
 module ShopHelper
   def parents(group)
     parents = []
+    group = group.category
     while group
       parents << group
       group = group.category
@@ -13,5 +14,15 @@ module ShopHelper
     prods = group.shop_products.to_a
     prods.delete(product)
     prods.sample(get)
+  end
+
+  # a short version of a desciption text. now we use markdown and often have a bold "header"
+  # so if we find a bold we use just that, regardless of length given
+  # otherwise we try and find a space close to the given length, to avoid ugly half-words
+  def short text , len = 110
+    ind = text.index( "**" , 10)  || text.index( "**" , 10)
+    return text[0 .. ind + 1] if ind
+    ind = text.index(" " , len - 10)
+    ind ? text[0 .. ind + 1] : text[0 .. len +1]
   end
 end
