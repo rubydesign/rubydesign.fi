e0ba077087f919ba0413060d78b1f87c85d210b9
Torsten Ruger <torsten@villataika.fi>
2014-11-29 11:58:51 +0200

    empty summary and description for prod and group page, start tests

--- spec/features/products/index_spec.rb
+++ spec/features/products/index_spec.rb
@@ -6,11 +6,33 @@ describe "GET /products" do
    end
   it "lists no products" do
     visit_path products_path
+    expect(all(".image").count).to be 0
   end
   it "filters by price" do
-    10.times { create :product }
+    create :product , :price => 8
+    create :product , :price => 12
     visit products_path
     fill_in "q[price_lteq]", :with => '10'
-    click_button( I18n.t(:filter))
+    expect(all(".image").count).to be 2
+    click_button( :filter)
+    expect(all(".image").count).to be 1
+  end
+  it "filters by online" do
+    create :product , :online => true
+    create :product , :online => false
+    visit products_path
+    expect(all(".image").count).to be 2
+    choose("q[online_eq]")
+    click_button( :filter)
+    expect(all(".image").count).to be 1
+  end
+  it "filters by empty summary" do
+    create :product , :summary => "i have a summary"
+    create :product , :summary => ""
+    visit products_path
+    expect(all(".image").count).to be 2
+    choose("q[summary_blank]")
+    click_button( :filter)
+    expect(all(".image").count).to be 1
   end
 end
