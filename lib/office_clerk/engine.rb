f934cf82a4b6341b860ca16c0ff8b2d6cc2559b2
Torsten Ruger <torsten@villataika.fi>
2014-12-08 21:13:34 +0200

    hopefully fix the load order issue

--- lib/office_clerk/engine.rb
+++ lib/office_clerk/engine.rb
@@ -1,3 +1,5 @@
+require "best_in_place"
+
 module OfficeClerk
   class Engine < ::Rails::Engine
     engine_name "office"
@@ -11,13 +13,16 @@ module OfficeClerk
 
     I18n.enforce_available_locales = false
     I18n.default_locale = :fi
+    # for testing this makes engine factories available
+    initializer "model_core.factories", :after => "factory_girl.set_factory_paths" do
+      FactoryGirl.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__) if defined?(FactoryGirl)
+    end
+
+    # have to init the BestInPlace first to be able to include helpers
+    config.railties_order = [BestInPlace::Railtie , OfficeClerk::Engine  , :all  ] 
     config.after_initialize do
       BestInPlace::ViewHelpers.extend OfficeHelper
     end
-    # for testing this makes engine factories available
-    initializer "model_core.factories", :after => "factory_girl.set_factory_paths" do
-       FactoryGirl.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__) if defined?(FactoryGirl)
-     end
 
     config.paperclip_defaults =  {  :styles => {:thumb => '48x48>', :list => '150x150>', :product  => '600x600>' },
                                 :default_style => :list,
