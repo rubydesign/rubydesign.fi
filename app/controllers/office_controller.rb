5a6a83c58c3c66eeac2966f54f4a78b017011645
Torsten Ruger <torsten@villataika.fi>
2014-12-18 21:11:10 +0200

    move redirects to session controller

--- app/controllers/office_controller.rb
+++ app/controllers/office_controller.rb
@@ -5,13 +5,6 @@ class OfficeController < ApplicationController
 
   include OfficeClerk::Engine.routes.url_helpers
 
-  def redirect_after_sign_up
-    redirect_to main_app.root_url, :notice => t(:signed_up)
-  end
-  def redirect_after_sign_in
-    redirect_to main_app.root_url, :notice => t(:signed_in)
-  end
-
   def error
     logger.info "Error" + request.url
     redirect_to "/"
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -38,6 +38,12 @@ class SessionsController < OfficeController
 
   private
 
+  def redirect_after_sign_up
+    redirect_to main_app.root_url, :notice => t(:signed_up)
+  end
+  def redirect_after_sign_in
+    redirect_to main_app.root_url, :notice => t(:signed_in)
+  end
   def params_for_clerk
     params.require(:clerk).permit(:email,:password,:password_confirmation)
   end
