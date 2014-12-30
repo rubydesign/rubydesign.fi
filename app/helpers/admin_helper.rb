08cae7829c047340988f1dc4009195603e66336b
Torsten Ruger <torsten@villataika.fi>
2014-12-30 18:22:59 +0200

    move helper code

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,23 +1,5 @@
 # encoding : utf-8
 module AdminHelper
-    
-  def basket_edit_link basket , options = {}
-    return "---" unless basket
-    return "" unless request.url.include?("basket")
-    text = t(:edit) + " " 
-    link = edit_basket_path(basket)
-    case basket.kori
-    when Order
-      text += I18n.t(:order)
-      link = order_path(basket.kori) rescue ""
-    when Purchase
-      text += I18n.t(:purchase)
-      link = purchase_path(basket.kori) rescue ""
-    else
-      text += t(:basket)
-    end
-    return link_to text , link , options 
-  end
   
   def sort_date key
     return "" unless params[:q]
--- app/helpers/baskets_helper.rb
+++ app/helpers/baskets_helper.rb
@@ -6,4 +6,25 @@ module BasketsHelper
     return false if styles.nil?
     return styles.split.include?("receipt")
   end
+  def basket_edit_link basket , options = {}
+    return "---" unless basket
+    return "" unless request.url.include?("basket")
+    if basket.locked?
+      text =  I18n.t(:locked) + ": "
+      case basket.kori
+      when Order
+        text += I18n.t(:order)
+        link = order_path(basket.kori) rescue ""
+      when Purchase
+        text += I18n.t(:purchase)
+        link = purchase_path(basket.kori) rescue ""
+      else
+        raise "System Error: Locked basket without order #{basket.id}"
+      end
+    else
+      text = t(:basket)
+      link = edit_basket_path(basket)
+    end
+    return link_to text , link , options 
+  end
 end
