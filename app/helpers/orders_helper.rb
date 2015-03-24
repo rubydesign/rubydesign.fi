6bc31cc791f4dab9d8580cfd44bfc493b77eb7fc
Torsten Ruger <torsten@villataika.fi>
2015-03-24 21:18:08 +0200

    removed the reference, which is now only in print and sales clerks

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -8,10 +8,6 @@ module OrdersHelper
   def mail_path action
     
   end
-  def viite
-    base = @order.number[1 .. -1]
-    viite = open("http://www1.nordea.fi/P636V/H636VTXT.asp?action=tekstiLista&lkm=1&alkuViite=#{base}&L=1").read[/\d+ \d+/]
-  end
   def number_with_comma n
     number_with_precision(n , :precision => 2 , :separator => "," , :strip_insignificant_zeros => false)
   end
