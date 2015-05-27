6bdbd2aea66ec3f197613535b55cad5065f5a89f
Torsten Ruger <torsten@villataika.fi>
2015-05-27 20:40:52 +0300

    add variable page titles (for bills mainly)

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,6 +1,6 @@
 # encoding : utf-8
 module AdminHelper
-  
+
   def sort_date key
     return "" unless params[:q]
     params[:q][key] || ""
@@ -12,10 +12,10 @@ module AdminHelper
   # a default note
   # same signature as best_in_place, ie object, field symbol , hash
   def in_place object , field , attributes ={}
-    defaults = { :ok_button => I18n.t(:edit), :ok_button_class => "btn btn-success" , 
+    defaults = { :ok_button => I18n.t(:edit), :ok_button_class => "btn btn-success" ,
                  :cancel_button => I18n.t(:cancel) , :cancel_button_class => "btn btn-warning",
                  :place_holder => I18n.t(:edit)  , :inner_class => "form-control" }
-    attributes.reverse_merge! defaults 
+    attributes.reverse_merge! defaults
     best_in_place(object , field , attributes)
   end
 
@@ -25,5 +25,10 @@ module AdminHelper
     engines.delete_if {|e| ! e.respond_to?(:office_assets) }
     assets = engines.collect{ |e| e.office_assets }
     assets.compact
+  end
+
+  # helper so one doesn't hav to write the funny content_for in templates
+  def title(page_title)
+    content_for(:title) { page_title }
   end
 end
