eb1c0437decb26511f536456c7fd83d4a9cda813
Torsten Ruger <torsten@villataika.fi>
2017-06-01 02:02:18 +0300

    delegate precompile to app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,12 +2,4 @@ Rails.application.config do
 
   config.assets.precompile += %w( office_clerk.js office_clerk.css)
 
-  (Rails::Engine.subclasses.map(&:instance) << Rails.application).each do |e|
-    next unless e.respond_to? :office_assets
-    asset = e.office_assets
-    next if asset.blank?
-    config.assets.precompile << (asset + ".js")
-    config.assets.precompile << (asset + ".css")
-  end
-
 end
