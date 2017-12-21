8f25d6a5cf381259bf732dc89f30d9a4ce5eb6c4
Torsten Ruger <torsten@villataika.fi>
2017-12-21 11:32:19 +0200

    fold emails into items
    too much difference on the js side otherwise

--- app/controllers/new_reporter.rb
+++ app/controllers/new_reporter.rb
@@ -8,16 +8,26 @@ module NewReporter
     @group_names = { "all" => "All" ,
                      "products.supplier_id" => t(:supplier) ,
                      "products.category_id" => t(:category) ,
-                     "product_id" => t(:product) }
+                     "product_id" => t(:product) ,
+                     "email" => t(:email) }
     @start = params[:start] ? Time.at(params[:start].to_i) : 3.months.ago.beginning_of_month
     @end =   params[:end]   ? Time.at(params[:end].to_i)   : Date.today.end_of_month
+    basket_index =   @attributes.index("basket_id")
+    created_index =  @attributes.index("created_at")
+    price_index =    @attributes.index("price")
+    quantity_index = @attributes.index("quantity")
+    emails = Basket.unscoped.where("baskets.created_at": @start..@end).
+                  includes(:order).where.not(locked: nil).
+                  pluck("baskets.id" , "orders.email").to_h
     @items = Item.where(created_at: @start..@end).
                   includes(:product).includes(:basket).
                   where.not(baskets: {locked: nil}).
-                  pluck(*@attributes).map{|i| i[-1] = i[-1].to_i*1000;i[0] = i[0]*i[1] ; i}
-    @orders = Basket.unscoped.where("baskets.created_at": @start..@end).
-                  includes(:order).where.not(locked: nil).
-                  pluck("baskets.id" , "orders.email").to_h
+                  pluck(*@attributes).map do|item|
+                    item[created_index] = item[created_index].to_i*1000
+                    item[price_index] = item[price_index]*item[quantity_index] 
+                    item << emails[ item[basket_index] ]
+                    item
+                  end
 
     respond_to do |format|
       format.html { render }
