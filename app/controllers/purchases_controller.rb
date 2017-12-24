7b0583fc917401a21a9c5a781ee3e29a7f4c979b
Torsten Ruger <torsten@villataika.fi>
2017-12-24 22:41:46 +0200

    small sql improvements

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -66,14 +66,16 @@ class PurchasesController < AdminController
 
   protected
   def create_data
-    @products = Product.where(supplier_id: @purchase.supplier_id).where("inventory > ?", 0)
+    @products = Product.where(supplier_id: @purchase.supplier_id).
+                        where("inventory > ?", 0).
+                        includes(:category)
     @ordered_products = Hash.new(0)
     last_purchase = Purchase.where(supplier_id: @purchase.supplier_id).
                     where("created_at < ?" ,@purchase.created_at).
                     order(created_at: :desc).first
     since = last_purchase  ? last_purchase.created_at : Time.now - 4.weeks
     @orders =  Order.where("created_at > ?" ,since).where(shipped_on: nil)
-    @orders.includes(basket: :items).each do |order|
+    @orders.includes(basket: {items: :product}).each do |order|
       order.basket.items.each{ |item| @ordered_products[item.product_id] += item.quantity}
     end
   end
